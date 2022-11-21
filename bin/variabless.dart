a018c8acffd3095dfcd489081662a2699ede0abb
CHINMAYJC <jcchinmay@gmail.com>
2022-11-21 15:14:26 +0530

    commit

---
+++ bin/variabless.dart
@@ -0,0 +1,19 @@
+import 'package:test/test.dart';
+
+void main(){
+  String name = "chinmay";
+  int    age = 23;
+  int   phone = 9846771047;
+  double mark = 8.3;
+  String mail = "jcchinmay@gmail.com";
+
+
+  print("---My Resume---");
+  print("my name is $name"); //string interpolation = ${variablename} my name is chinmay
+  print("my age is $age");
+  print("my phone number is $phone");
+  print("my cgpa is $mark");
+  print ("my email is $mail");
+
+
+}
